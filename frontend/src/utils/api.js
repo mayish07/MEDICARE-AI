912f4db82afdfc04efc3176999f02c7eac3badd3
mayish <mayish07@gmail.com>
2026-04-27 09:51:31 +0100

    Fixed all storage errors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -10,10 +10,12 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (e) {}
   return config;
 });
 
@@ -21,9 +23,10 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
+      try {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+      } catch (e) {}
     }
     return Promise.reject(error);
   }
